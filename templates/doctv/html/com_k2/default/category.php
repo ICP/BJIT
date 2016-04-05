25633d419ea553f8716e1f766c175d547e9c90c4
Farid Rn <faridv@gmail.com>
2016-04-05 20:58:56 +0430

    Moved items in next pages to secondary group

--- templates/doctv/html/com_k2/default/category.php
+++ templates/doctv/html/com_k2/default/category.php
@@ -8,6 +8,30 @@
  */
 // no direct access
 defined('_JEXEC') or die;
+
+$request = JFactory::getApplication()->input;
+$limitstart = $request->getInt('limitstart', null);
+if ($limitstart && $limitstart > 0) {
+	$itemlist = array();
+	if (isset($this->leading) && count($this->leading)) {
+		foreach ($this->leading as $k => $v) {
+			$itemlist[] = $v;
+		}
+		$this->leading = null;
+	}
+	if (isset($this->primary) && count($this->primary)) {
+		foreach ($this->primary as $k => $v) {
+			$itemlist[] = $v;
+		}
+		$this->primary = null;
+	}
+	if (isset($this->secondary) && count($this->secondary)) {
+		foreach ($this->secondary as $k => $v) {
+			$itemlist[] = $v;
+		}
+		$this->secondary = $itemlist;
+	}
+}
 ?>
 <div id="category" class="itemlist<?php if ($this->params->get('pageclass_sfx')) echo ' ' . $this->params->get('pageclass_sfx'); ?>">
 	<?php if ((isset($this->leading) || isset($this->primary) || isset($this->secondary) || isset($this->links)) && (count($this->leading) || count($this->primary) || count($this->secondary) || count($this->links))) { ?>
@@ -42,7 +66,8 @@ defined('_JEXEC') or die;
 		<?php } ?>
 		<?php if (isset($this->secondary) && count($this->secondary)) { ?>
 			<div class="box articles highlights">
-				<?php foreach ($this->secondary as $key => $item) { 
+				<?php
+				foreach ($this->secondary as $key => $item) {
 					$media = !empty($item->video) ? ' video' : '';
 					$media .=!empty($item->gallery) ? ' gallery' : '';
 					$modified = ($item->modified != $this->nullDate && $item->modified != $item->created) ? JHTML::_('date', $item->modified, JText::_('K2_DATE_FORMAT_LC2')) : '';
@@ -60,12 +85,13 @@ defined('_JEXEC') or die;
 			<div class="box list-items">
 				<h4><?php echo JText::_('K2_MORE'); ?></h4>
 				<ul>
-					<?php foreach ($this->links as $key => $item) { 
-					$media = !empty($item->video) ? ' video' : '';
-					$media .=!empty($item->gallery) ? ' gallery' : '';
-					$modified = ($item->modified != $this->nullDate && $item->modified != $item->created) ? JHTML::_('date', $item->modified, JText::_('K2_DATE_FORMAT_LC2')) : '';
-					?>
-					<li class="<?php echo $media; ?><?php echo ($item->featured) ? ' featured' : ''; ?><?php if ($item->params->get('pageclass_sfx')) echo ' ' . $item->params->get('pageclass_sfx'); ?>" data-hits="<?php echo $item->hits; ?>">
+					<?php
+					foreach ($this->links as $key => $item) {
+						$media = !empty($item->video) ? ' video' : '';
+						$media .=!empty($item->gallery) ? ' gallery' : '';
+						$modified = ($item->modified != $this->nullDate && $item->modified != $item->created) ? JHTML::_('date', $item->modified, JText::_('K2_DATE_FORMAT_LC2')) : '';
+						?>
+						<li class="<?php echo $media; ?><?php echo ($item->featured) ? ' featured' : ''; ?><?php if ($item->params->get('pageclass_sfx')) echo ' ' . $item->params->get('pageclass_sfx'); ?>" data-hits="<?php echo $item->hits; ?>">
 							<?php
 							$this->item = $item;
 							echo $this->loadTemplate('item');
@@ -76,10 +102,8 @@ defined('_JEXEC') or die;
 			</div>
 		<?php } ?>
 		<?php if ($this->pagination->getPagesLinks()) { ?>
-			<div class="paging">
-				<?php if ($this->params->get('catPagination')) echo $this->pagination->getPagesLinks(); ?>
-				<?php if ($this->params->get('catPaginationResults')) echo $this->pagination->getPagesCounter(); ?>
-			</div>
+			<div class="text-center"><?php if ($this->params->get('catPaginationResults')) echo $this->pagination->getPagesCounter(); ?></div>
+			<nav><?php if ($this->params->get('catPagination')) echo $this->pagination->getPagesLinks(); ?></nav>
 		<?php } ?>
 	<?php } ?>
 </div>
