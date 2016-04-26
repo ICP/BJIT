544900ba505adee2e607652188e93e004b6df707
Farid Rn <faridv@gmail.com>
2016-04-26 18:15:43 +0430

    Updated Joomla and some cleanups

---
+++ logs/joomla_update.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-04-17 17:53:52 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-04-17T17:53:52+00:00	INFO ::1	update	Update started by user Super User (575). Old version is 3.5.0.
+2016-04-17T17:53:53+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.5.1/Joomla_3.5.x_to_3.5.1-Stable-Patch_Package.zip.
+2016-04-17T17:53:56+00:00	INFO ::1	update	File Joomla_3.5.x_to_3.5.1-Stable-Patch_Package.zip successfully downloaded.
+2016-04-17T17:53:56+00:00	INFO ::1	update	Starting installation of new version.
+2016-04-17T17:53:57+00:00	INFO ::1	update	Finalising installation.
+2016-04-17T17:53:57+00:00	INFO ::1	update	Ran query from file 3.5.1-2016-03-25. Query text: ALTER TABLE `#__user_keys` MODIFY `user_id` varchar(150) NOT NULL;.
+2016-04-17T17:53:57+00:00	INFO ::1	update	Ran query from file 3.5.1-2016-03-29. Query text: UPDATE `#__utf8_conversion` SET `converted` = 0  WHERE (SELECT COUNT(*) FROM `#_.
+2016-04-17T17:53:57+00:00	INFO ::1	update	Deleting removed files and folders.
+2016-04-17T17:54:03+00:00	INFO ::1	update	Cleaning up after installation.
+2016-04-17T17:54:03+00:00	INFO ::1	update	Update to version 3.5.1 is complete.
