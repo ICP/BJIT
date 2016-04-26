778b0a63570504d9fe8b0c887fe7065b03851b42
Farid Rn <faridv@gmail.com>
2016-04-26 18:26:19 +0430

    Working on assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ $(function () {
     });
     $(".search-toggle").click(function () {
         $("html").toggleClass('no-scroll');
-    })
+    });
 
     $(".item-image[data-video]").each(function () {
         var poster = $(this).find("img:first").attr('src');
@@ -51,9 +51,33 @@ $(function () {
             file: file,
             image: poster,
             width: "100%",
-            aspectratio: "16:9",
+            aspectratio: "16:9"
         });
     });
+    
+    $(".nano").nanoScroller({
+        alwaysVisible: true
+    });
+
+    var $tilesTmpl = '<li><figure class="img"><a href="{link}"><img src="{img}" alt="{title}" /></a></figure><div class="desc"><div class="item-header"><h3 class="item-title"><a href="{link}">{title}</a></h3></div><time class="created">{created}</time></div></li>';
+    if ($(".item-boxes .box.more").length) {
+        var moreItems = $(".item-boxes .box.more");
+        var itemsContainer = moreItems.find("ul");
+        var url = moreItems.attr("data-catlink");
+        $.ajax({
+            url: url
+            , data: 'format=json'
+            , success: function(d) {
+                if (typeof d === "object") {
+                    items = d.items;
+                    $.each(items, function() {
+                        item = $tilesTmpl.replace(/{link}/g, this.link).replace(/{img}/g, this.image).replace(/{title}/g, this.title).replace(/{created}/g, this.time);
+                        itemsContainer.append(item);
+                    });
+                }
+            }
+        });
+    }
 
     $(".box.gallery").on('click', ".thumbnail", function (e) {
         var imgPath = $(this).attr('href');
@@ -82,11 +106,10 @@ $(function () {
         , itemClass: 'item'
         , animateOut: ''
         , nav: true
-        , navText: ["", ""]
+        , navText: ["", ""]
     });
-    
-    
-    
+
+
     $(".box.thumbs.carousel > div ul").owlCarousel({
         margin: 0
         , loop: true
@@ -125,49 +148,4 @@ $(function () {
     });
     $(".full-height").height(fullHeightHeught);
 
-//    $(".grayscale.color-on-hover figure").hover(
-//            function () {
-//                $(this).find('.original').stop().animate({opacity: 1}, 500);
-//            },
-//            function () {
-//                $(this).find('.original').stop().animate({opacity: 0}, 500);
-//            }
-//    );
 });
-
-$(window).load(function () {
-    $('.grayscale img').each(function () {
-        var $wrapper = '<div style="position: relative; display:inline-block;width:' + this.width + 'px;height:' + this.height + 'px;">';
-        $(this).wrap($wrapper).clone().addClass('original').css({'position': 'absolute', 'opacity': 0}).insertBefore(this);
-        this.src = grayscale(this.src);
-        if ($(this).is(":hidden"))
-            $(this).fadeIn(500);
-    });
-});
-
-// http://net.tutsplus.com/tutorials/javascript-ajax/how-to-transition-an-image-from-bw-to-color-with-canvas/
-function grayscale(src) {
-    var supportsCanvas = !!document.createElement('canvas').getContext;
-    if (supportsCanvas) {
-        var canvas = document.createElement('canvas'),
-                context = canvas.getContext('2d'),
-                imageData, px, length, i = 0, gray,
-                img = new Image();
-        img.src = src;
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0);
-        imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        px = imageData.data;
-        length = px.length;
-
-        for (; i < length; i += 4) {
-            gray = px[i] * .3 + px[i + 1] * .59 + px[i + 2] * .11;
-            px[i] = px[i + 1] = px[i + 2] = gray;
-        }
-        context.putImageData(imageData, 0, 0);
-        return canvas.toDataURL();
-    } else {
-        return src;
-    }
-}
