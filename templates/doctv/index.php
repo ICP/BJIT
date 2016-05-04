12726ee38ddd45066d09a40929522295c5a1bbb1
Farid Rn <faridv@gmail.com>
2016-05-04 21:00:37 +0430

    Minor security improvements

--- templates/doctv/index.php
+++ templates/doctv/index.php
@@ -3,6 +3,7 @@ defined('_JEXEC') or die('Restricted access');
 
 if (!JFactory::getApplication()->getMenu()->getActive()) {
 	header('Location: ' . JURI::base(), true, 303);
+	exit();
 }
 
 include 'blocks/header.php';
