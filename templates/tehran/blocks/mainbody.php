4879a419c6856c8d03aab7bcc1a3f1f1abad64a6
Farid Rn <faridv@gmail.com>
2016-05-06 19:01:37 +0430

    Working on Tehran template

--- templates/tehran/blocks/mainbody.php
+++ templates/tehran/blocks/mainbody.php
@@ -1,37 +1,38 @@
 <body id="bd" class="<?php echo $theme . ' ' . strtolower($helper->device); ?>" data-spy="scroll" data-target="#menu">
 	<header id="header">
 		<?php if ($isFrontpage) { ?><h1 class="hide"><?php echo $sitename; ?></h1><?php } ?>
-		<div class="wrapper gray-darkest">
+		<div id="masthead" class="wrapper purple-light">
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-12">
-						<div class="header-schedule">
-							<a class="schedule-toggle" href="<?php echo JURI::base() . 'schedule'; ?>" data-toggle="toggle" data-target="#schedule"><i class="icon-play"></i><span>جدول پخش</span></a>
-						</div>
 						<div class="logo">
 							<a href="<?php echo JURI::base(); ?>" title="<?php echo $sitename; ?>"><?php echo $sitename; ?></a>
 						</div>
-						<?php if ($helper->countModules('menu')) { ?>
-							<jdoc:include type="modules" name="menu" />
-						<?php } ?>
 						<div class="menu-toggle hidden-md hidden-lg"><a href="#" data-toggle="menuslide" data-target=".menu"><i class="icon-menu"></i></a></div>
 						<div class="search hidden-sm hidden-xs">
 							<a class="search-toggle" href="<?php echo JURI::base() . 'search'; ?>" data-toggle="toggle" data-target="#search" data-focus="#search input[type='text']"><i class="icon-search"></i></a>
 						</div>
 						<ul class="header-anchors list-inline list-unstyled">
-							<li class="lang-anchor">
-								<a href="<?php echo JURI::base() . 'english'; ?>"><span>English</span></a>
-							</li>
-							<li class="live-anchor">
-								<a href="<?php echo JURI::base() . 'live'; ?>"><i class="icon-live"></i><span>پخش زنده</span></a>
-							</li>
-							<li class="catalog-anchor">
-								<a href="#"><i class="icon-download"></i><span>کاتالوگ</span></a>
-							</li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-aparat"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+							<li><a href="#"><i class="icon-telegram"></i></a></li>
 							<li class="search hidden-md hidden-lg">
 								<a class="search-toggle" href="<?php echo JURI::base() . 'search'; ?>" data-toggle="toggle" data-target="#search" data-focus="#search input[type='text']"><i class="icon-search"></i><span>جستجو</span></a>
 							</li>
 						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="navbar" class="wrapper purple">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<?php if ($helper->countModules('menu')) { ?>
+							<jdoc:include type="modules" name="menu" />
+						<?php } ?>
 					</div>
 				</div>
 			</div>
@@ -44,13 +45,6 @@
 			<jdoc:include type="modules" name="showcase" style="basic" />
 		</div>
 	</section>
-<?php } ?>
-<?php if ($helper->countModules('special')) { ?>
-	<aside id="showcase">
-		<div class="wrapper yellow">
-			<jdoc:include type="modules" name="special" style="basic" />
-		</div>
-	</aside>
 <?php } ?>
 <main id="mainbody">
 	<div class="wrapper content gray-dark">
@@ -85,80 +79,75 @@
 					</div>
 				</div>
 			<?php } ?>
-			<?php if ($helper->countModules('content-left') || $helper->countModules('content-center') || $helper->countModules('content-right')) { ?>
-				<div class="container">
-					<div id="contents" class="row">
-						<?php if ($helper->countModules('content-right')) { ?>
-							<div id="content-right" class="col-xs-12 col-md-4"><jdoc:include type="modules" name="content-right" style="default" /></div>
-						<?php } ?>
-						<?php if ($helper->countModules('content-center')) { ?>
-							<div id="content-center" class="col-xs-12 col-md-4"><jdoc:include type="modules" name="content-center" style="default" /></div>
-						<?php } ?>
-						<?php if ($helper->countModules('content-left')) { ?>
-							<div id="content-left" class="col-xs-12 col-md-4"><jdoc:include type="modules" name="content-left" style="default" /></div>
-						<?php } ?>
+			<div class="container">
+				<?php if ($helper->countModules('main-top')) { ?>
+					<div id="main-top" class="row">
+						<div class="col-xs-12">
+							<jdoc:include type="modules" name="main-top" style="default" />
+						</div>
 					</div>
-				</div>
-			<?php } ?>
-		</div>
-		<?php if ($helper->countModules('main-top-left') || $helper->countModules('main-top-right')) { ?>
-			<div class="wrapper main-top gray-darker">
-				<div class="container">
-					<div class="row">
-						<?php if ($helper->countModules('main-top-right')) { ?>
-							<div id="main-top-right" class="col-xs-12 col-sm-6"><jdoc:include type="modules" name="main-top-right" style="default" /></div>
-						<?php } ?>
-						<?php if ($helper->countModules('main-top-left')) { ?>
-							<div id="main-top-left" class="col-xs-12 col-sm-6"><jdoc:include type="modules" name="main-top-left" style="default" /></div>
-						<?php } ?>
+				<?php } ?>
+				<?php if ($helper->countModules('main')) { ?>
+					<div id="main" class="row">
+						<div class="col-xs-12">
+							<jdoc:include type="modules" name="main" style="default" />
+						</div>
 					</div>
-				</div>
-			</div>
-		<?php } ?>
-		<?php if ($helper->countModules('main-left') || $helper->countModules('main-right')) { ?>
-			<div class="wrapper main gray-dark">
-				<div class="container">
-					<div class="row">
-						<?php if ($helper->countModules('main-right')) { ?>
-							<div  id="main-right" class="col-xs-12 col-sm-6 full-height"><jdoc:include type="modules" name="main-right" style="default" /></div>
-						<?php } ?>
-						<?php if ($helper->countModules('main-left')) { ?>
-							<div id="main-left" class="col-xs-12 col-sm-6 full-height"><jdoc:include type="modules" name="main-left" style="default" /></div>
-						<?php } ?>
+				<?php } ?>
+				<?php if ($helper->countModules('main-bot')) { ?>
+					<div id="main-bot" class="row">
+						<div class="col-xs-12">
+							<jdoc:include type="modules" name="main-bot" style="default" />
+						</div>
 					</div>
-				</div>
-			</div>
-		<?php } ?>
-		<?php if ($helper->countModules('main-bot')) { ?>
-			<div class="wrapper main-bot gray">
-				<jdoc:include type="modules" name="main-bot" />
-			</div>
-		<?php } ?>
-</main>
-<footer id="footer">
-	<div class="wrapper gray-darker">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-md-3">
-					<div class="footer-about">
-						<a href="<?php echo JURI::base(); ?>"><img src="<?php echo JURI::base() ?>assets/img/logo_footer.png" /></a>
-						<?php if ($helper->countModules('footer-about')) { ?><jdoc:include type="modules" name="footer-about" /><?php } ?>
-					</div>
-				</div>
-				<div class="col-xs-12 col-md-9">
-					<?php if ($helper->countModules('footer')) { ?><jdoc:include type="modules" name="footer" /><?php } ?>
-					<?php if ($helper->countModules('copyright')) { ?><jdoc:include type="modules" name="copyright" /><?php } ?>
-				</div>
+				<?php } ?>
 			</div>
 		</div>
 	</div>
-	<?php if ($helper->countMessages($app->getMessageQueue())) { ?><jdoc:include type="message" /><?php } ?>
+</main>
+<footer id="footer">
+	<?php if ($helper->countModules('newsletter')) { ?>
+		<div id="newsletter" class="wrapper gray-darker">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<jdoc:include type="modules" name="newsletter" />
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
+	<?php if ($helper->countModules('sitemap')) { ?>
+		<div id="sitemap" class="wrapper gray-dark">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-10 col-md-9">
+						<jdoc:include type="modules" name="footer" />
+					</div>
+					<div class="col-xs-12 col-sm-2 col-md-3">
+						<a href="<?php echo JURI::base(); ?>"><img src="<?php echo JURI::base() ?>assets/img/logo_footer.png" /></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
+	<?php if ($helper->countModules('copyright')) { ?>
+		<div id="copyright" class="wrapper purple">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<jdoc:include type="modules" name="copyright" />
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
 </footer>
 <script src="<?php echo JURI::base(); ?>assets/js/jquery-1.11.1.min.js"></script>
 <script src="<?php echo JURI::base(); ?>assets/js/bootstrap.min.js"></script>
-<script src="<?php echo JURI::base(); ?>assets/js/owl.carousel.js"></script>
+<script src="<?php echo JURI::base(); ?>assets/js/owl.carousel.min.js"></script>
 <script src="<?php echo JURI::base(); ?>assets/js/jquery.nanoscroller.min.js"></script>
-<script src="<?php echo JURI::base(); ?>assets/js/main.js?_=20160326"></script>
+<script src="<?php echo JURI::base(); ?>assets/js/tehran.min.js?_=20160326"></script>
 <script type="text/javascript">
     // Piwik code
 </script>
