4b49663968eabc2ed3496516b81ad6686fe13ee6
Farid Rn <faridv@gmail.com>
2016-05-31 19:01:16 +0430

    Added FAQ module template

---
+++ templates/doctv/html/mod_content/Faq/default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @version		$Id: default.php 1251 2011-10-19 17:50:13Z joomlaworks $
+ * @package		K2
+ * @author		JoomlaWorks http://www.joomlaworks.gr
+ * @copyright	Copyright (c) 2006 - 2011 JoomlaWorks Ltd. All rights reserved.
+ * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+?>
+<?php if (count($items)) { ?>
+	<?php if ($params->get('category_link') && $params->get('mymenu_id')) { ?>
+		<a href="<?php echo JRoute::_('index.php?option=com_k2&Itemid=' . $params->get('mymenu_id')); ?>" class="btn btn-default category-link">سوالات دیگر</a>
+	<?php } ?>
+	<ul>
+		<?php foreach ($items as $key => $item) { ?>
+			<li>
+				<div class="desc">
+					<h3><a href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a></h3>
+					<?php echo $item->introtext; ?>
+				</div>
+			</li>
+		<?php } ?>
+	</ul>
+<?php } ?>
