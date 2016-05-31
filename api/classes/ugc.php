71a6a238ec02a892987eaedf22d251794e1534c5
Farid Rn <faridv@gmail.com>
2016-05-31 18:57:27 +0430

    Added UGC API

---
+++ api/classes/ugc.php
@@ -0,0 +1,73 @@
+<?php
+
+Class UGC {
+
+	// Constructor
+	function UGC($type = null, $app, $method) {
+		switch ($type) {
+			case 'requesttitle':
+				if ($method == "post")
+					self::requestTitle($app);
+				break;
+			case 'upload':
+				if ($method == "post")
+					self::add($app);
+				break;
+			case 'file':
+				self::file($app);
+		}
+	}
+
+	public static function add($app) {
+		$user = JFactory::getUser();
+		if ($user->guest) {
+			$app->render(403, array('error' => true, 'msg' => 'Not authorized', 'status' => 403));
+		}
+		$o = new stdClass();
+		$o->created = date('Y-m-d H:i:s', time());
+		$o->created_by = ($user->id ? $user->id : 0);
+		$o->ordering = $app->_db->quote(0);
+		$o->state = $app->_db->quote(0);
+		$o->title = $app->request->post('title', "");
+		$o->description = $app->request->post('description', "");
+		$o->file = $app->request->post('file', "");
+		$o->params = "";
+		
+		$app->_db->getQuery(true);
+		try {
+			$results = $app->_db->insertObject('#__ugc_items', $o);
+			$id = $app->_db->insertid(); // TODO
+		} catch (Exception $ex) {
+			$app->render(500, array('error' => true, 'msg' => 'خطایی رخ داده است'));
+		}
+		$app->render(200, array('db' => $results, 'item' => $o, 'msg' => 'اثر شما با موفقیت ثبت و پس از بررسی روی سایت منتشر خواهد شد'));
+	}
+
+	public static function file($app) {
+		include dirname(dirname(__FILE__)) . DS . 'helpers' . DS . 'uploader' . DS . 'endpoint.php';
+		die();
+	}
+
+	public static function requestTitle($app) {
+		$id = $app->request->post('item_id', null);
+		$user = JFactory::getUser();
+		if ($id) {
+			$o = new stdClass();
+			$o->created = date('Y-m-d H:i:s', time());
+			$o->created_by = $app->_db->quote(($user->id ? $user->id : 0));
+			$o->item_id = $app->_db->quote($id);
+
+			$app->_db->getQuery(true);
+			try {
+				$results = $app->_db->insertObject('#__ugc_requesttitles', $o);
+				$id = $app->_db->insertid();
+			} catch (Exception $ex) {
+				$app->render(500, array('error' => true, 'msg' => 'به نظر می‌رسد شما قبلاً این برنامه را معرفی کرده‌اید'));
+			}
+			$app->render(200, array('db' => $results, 'item' => $o, 'msg' => 'درخواست شما با موفقیت ثبت شد'));
+		} else {
+			$app->render(500, array('error' => true, 'msg' => 'Wrong item id', 'status' => 500));
+		}
+	}
+
+}
--- api/routes.php
+++ api/routes.php
@@ -7,13 +7,13 @@ $app->get('/', function () use ($app) {
 	$app->render(200, array('msg' => 'Welcome' . ' ' . $name));
 });
 
-// Convert
+// Converts
 $app->map('/convert/:type(/)', function ($type) use ($app) {
 	include 'classes/convert.php';
 	new Convert($app, $type);
 })->via('GET');
 
-// Content
+// Programs & Episodes
 $app->map('/programepisodes(/)', function () use ($app) {
 	Items::getProgramsList($app);
 })->via('GET')->via('POST');
@@ -25,10 +25,10 @@ $app->map('/programepisodes(/)(:catid(/))', function ($catid) use ($app) {
 	}
 })->via('GET');
 
+// Content
 $app->map('/homepage(/)', function () use ($app) {
 	Items::getItems($app, 'homepage');
 })->via('GET');
-
 $app->map('/items(/)(:id(/))', function ($id = null) use ($app) {
 	if (!$id) {
 		Items::getItems($app, 'items');
@@ -37,43 +37,19 @@ $app->map('/items(/)(:id(/))', function ($id = null) use ($app) {
 	}
 })->via('GET');
 
+// Schedules
 $app->map('/schedules(/)(:date(/))', function ($date = null) use ($app) {
 	$date = (!$date) ? date('Y-m-d', time()) : $date;
 	Schedule::getItems($app, $date);
 })->via('GET');
-
-$app->map('/user(/)', function () use ($app) {
-	jimport('joomla.user.helper');
-	$user = JFactory::getUser();
-	$session = JFactory::getSession();
-	$table =  JTable::getInstance('session');
-	$table->load( $session->getId() );
-
-//	if( $table->client_id == 1) {
-//	   echo 'admin_user';
-//	} else {
-//	   echo 'site_user';
-//	}
-	if ($session->get('admin_user') == 1) {
-   echo "Access granted admin";// for testing purposes
-} else if ($session->get('admin_user') == 0) {
-   echo "Access granted site";// for testing purposes
-} else {
-   echo "Access denied"; // for testing purposes
-}
-
-	$app->render(403, array('msg' => 'Not authorized','user'=>$user, 'session' => $session, 'table' => $table));
-})->via('GET');
-
 $app->map('/schedules(/)(:date(/))', function ($date = null) use ($app) {
 	$user = JFactory::getUser();
 	if ($user->authorise('core.edit', 'com_content')) {
 		$date = (!$date) ? date('Y-m-d', time()) : $date;
 		Schedule::add($app, $date);
 	}
-	$app->render(403, array('msg' => 'Not authorized','user'=>$user));
+	$app->render(403, array('msg' => 'Not authorized', 'user' => $user));
 })->via('POST');
-
 $app->map('/schedules(/)', function ($id = null) use ($app) {
 	if (!$id) {
 		$app->render(404, array('msg' => 'Item not found id =' . ' ' . $id));
@@ -83,67 +59,25 @@ $app->map('/schedules(/)', function ($id = null) use ($app) {
 //	Schedule::getItems($app, $date);
 })->via('PUT');
 
+// Users
+$app->map('/user(/)', function () use ($app) {
+	jimport('joomla.user.helper');
+	$user = JFactory::getUser();
+	$session = JFactory::getSession();
+	$table = JTable::getInstance('session');
+	$table->load($session->getId());
 
+	if ($session->get('admin_user') == 1) {
+		echo "Access granted admin"; // for testing purposes
+	} else if ($session->get('admin_user') == 0) {
+		echo "Access granted site"; // for testing purposes
+	} else {
+		echo "Access denied"; // for testing purposes
+	}
 
-// Misc
-//$app->map('/content/', function () use ($app) {
-//	$user = JFactory::getUser();
-//	if (count($user->getAuthorisedCategories('com_content', 'core.create')) > 0) {
-//		$row = new stdClass();
-//		$row->title = $app->_input->get('title');
-//		$row->introtext = $app->_input->get('introtext');
-//		$row->created_by = $user->id;
-//		$row->state = '1';
-//
-//		$app->_db->insertObject('#__content', $row);
-//
-//		$app->render(200, array(
-//			'msg' => $row->title . ' created!',
-//				)
-//		);
-//	}
-//
-//	$app->render(403, array(
-//		'msg' => 'Not authorized',
-//			)
-//	);
-//}
-//)->via('POST');
-//
-//$app->map('/content/:id', function ($id) use ($app) {
-//
-//	$user = JFactory::getUser();
-//	if ($user->authorise('core.edit', 'com_content.article.' . $id) || $user->authorise('core.edit.own', 'com_content.article.' . $id)) {
-//		$row = new stdClass();
-//		$row->id = $id;
-//		$row->title = $app->_input->get('title');
-//		$row->introtext = $app->_input->get('introtext');
-//		$row->state = '1';
-//
-//		$result = $app->_db->updateObject('#__content', $row, 'id');
-//
-//		$app->render(200, array(
-//			'msg' => $result,
-//				)
-//		);
-//	}
-//
-//	$app->render(403, array(
-//		'msg' => 'Not authorized',
-//			)
-//	);
-//}
-//)->via('PUT');
-//
-//$app->map('/content/:id', function ($id) use ($app) {
-//	$query = $app->_db->getQuery(true);
-//	$query->delete($app->_db->quoteName('#__content'))
-//			->where('id = ' . $app->_db->quote($id));
-//	$app->_db->setQuery($query);
-//
-//	$app->render(200, array(
-//		'msg' => $app->_db->query(),
-//			)
-//	);
-//}
-//)->via('DELETE');
+	$app->render(403, array('msg' => 'Not authorized', 'user' => $user, 'session' => $session, 'table' => $table));
+})->via('GET');
+$app->map('/ugc(/)(:type(/))', function ($type = null) use ($app) {
+	include 'classes/ugc.php';
+	new UGC($type, $app, 'post');
+})->via('POST');
