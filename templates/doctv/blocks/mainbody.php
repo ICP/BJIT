cce3533240dbb63bb869d6be1fdebd7e698a2ca4
Farid Rn <faridv@gmail.com>
2016-06-03 00:48:19 +0430

    Added Piwik statistics gathering code

--- templates/doctv/blocks/mainbody.php
+++ templates/doctv/blocks/mainbody.php
@@ -181,6 +181,20 @@
 <script src="<?php echo JURI::base(); ?>assets/js/owl.carousel.min.js"></script>
 <script src="<?php echo JURI::base(); ?>assets/js/doctv.min.js?_=20160326"></script>
 <script type="text/javascript">
-    // Piwik code
+    var _paq = _paq || [];
+    _paq.push(["trackPageView"]);
+    _paq.push(["enableLinkTracking"]);
+
+    (function () {
+        var u = (("https:" == document.location.protocol) ? "https" : "http") + "://93.190.24.112/piwik/";
+        _paq.push(["setTrackerUrl", u + "piwik.php"]);
+        _paq.push(["setSiteId", "1"]);
+        var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
+        g.type = "text/javascript";
+        g.defer = true;
+        g.async = true;
+        g.src = u + "piwik.js";
+        s.parentNode.insertBefore(g, s);
+    })();
 </script>
 </body>
