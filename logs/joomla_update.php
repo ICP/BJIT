004a0f26cd7f4cb237887ba51640d419d96919fb
Farid Rn <faridv@gmail.com>
2016-08-04 17:38:50 +0430

    Updated Joomla (reverted updated codehack files)

--- logs/joomla_update.php
+++ logs/joomla_update.php
@@ -36,3 +36,25 @@
 2016-07-14T09:40:29+00:00	INFO ::1	update	Deleting removed files and folders.
 2016-07-14T09:40:30+00:00	INFO ::1	update	Cleaning up after installation.
 2016-07-14T09:40:30+00:00	INFO ::1	update	Update to version 3.6.0 is complete.
+2016-08-04T13:02:32+00:00	INFO ::1	update	Update started by user فرید (575). Old version is 3.6.0.
+2016-08-04T13:02:34+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.6.1/Joomla_3.6.x_to_3.6.1-Stable-Patch_Package.zip.
+2016-08-04T13:02:46+00:00	INFO ::1	update	File Joomla_3.6.x_to_3.6.1-Stable-Patch_Package.zip successfully downloaded.
+2016-08-04T13:02:47+00:00	INFO ::1	update	Starting installation of new version.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Finalising installation.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `name` = 'Joomla! Core' WHERE `name` = 'Joomla Core.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `name` = 'Joomla! Extension Directory' WHERE `name`.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/core/list.x.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/jed/list.xm.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/language/tr.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-01. Query text: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/core/extens.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-06. Query text: ALTER TABLE `#__redirect_links` MODIFY `new_url` VARCHAR(2048);.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-08. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-08. Query text: UPDATE `#__update_sites_extensions` SET `extension_id` = 802 WHERE `update_site_.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-04-09. Query text: ALTER TABLE `#__menu_types` ADD COLUMN `asset_id` INT(11) NOT NULL AFTER `id`;.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-05-06. Query text: DELETE FROM `#__extensions` WHERE `type` = 'library' AND `element` = 'simplepie'.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-05-06. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-06-01. Query text: UPDATE `#__extensions` SET `protected` = 1, `enabled` = 1  WHERE `name` = 'com_a.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Ran query from file 3.6.0-2016-06-05. Query text: ALTER TABLE `#__languages` ADD COLUMN `asset_id` INT(11) NOT NULL AFTER `lang_id.
+2016-08-04T13:02:49+00:00	INFO ::1	update	Deleting removed files and folders.
+2016-08-04T13:02:51+00:00	INFO ::1	update	Cleaning up after installation.
+2016-08-04T13:02:51+00:00	INFO ::1	update	Update to version 3.6.1 is complete.
