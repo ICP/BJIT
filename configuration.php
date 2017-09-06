5de50d63331b633cfaf686b9ff2b273a55f056ea
Farid Rn <faridv@gmail.com>
2017-09-06 15:29:20 +0430

    Working on maximum error reporting environment

--- configuration.php
+++ configuration.php
@@ -20,7 +20,7 @@ class JConfig {
 	public $live_site = '';
 	public $secret = 'f5C2O6Wv0TCMgNbG';
 	public $gzip = '0';
-	public $error_reporting = 'maximum';
+	public $error_reporting = 'development';
 	public $helpurl = 'https://help.joomla.org/proxy?keyref=Help{major}{minor}:{keyref}&lang={langcode}';
 	public $ftp_host = '';
 	public $ftp_port = '';
