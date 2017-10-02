e57006e089ba81afd6c2c33222b12457b07e987d
Farid Rn <faridv@gmail.com>
2017-10-02 13:01:41 +0330

    Working on language-selector module

--- templates/gilgamesh/html/mod_languages/default.php
+++ templates/gilgamesh/html/mod_languages/default.php
@@ -7,41 +7,15 @@
  */
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-//JHtml::_('stylesheet', 'mod_languages/template.css', array(), true);
 ?>
-<?php if ($headerText) : ?>
-	<div class="pretext"><p><?php echo $headerText; ?></p></div>
-<?php endif; ?>
-<?php if ($params->get('dropdown', 1)) : ?>
-	<form name="lang" method="post" action="<?php echo JURI::current(); ?>">
-		<select class="inputbox" onchange="document.location.replace(this.value);" >
-			<?php foreach ($list as $language): ?>
-				<option dir=<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? '"rtl"' : '"ltr"' ?> value="<?php echo $language->link; ?>" <?php echo $language->active ? 'selected="selected"' : '' ?>>
-					<?php echo $language->title_native; ?></option>
-			<?php endforeach; ?>
-		</select>
-	</form>
-<?php else : ?>
-	<ul>
-		<?php foreach ($list as $language): ?>
-			<?php if ($params->get('show_active', 0) || !$language->active): ?>
-				<li class="<?php echo $language->active ? 'active' : ''; ?>" dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr' ?>">
-					<a href="<?php echo $language->link; ?>">
-						<?php if ($params->get('image', 1)): ?>
-							<?php echo JHtml::_('image', 'mod_languages/' . $language->image . '.gif', $language->title_native, array('title' => $language->title_native), true); ?>
-						<?php else : ?>
-							<?php
-								$langCode = $language->sef == "fa" ? 'فا' : $language->sef;
-								echo $params->get('full_name', 1) ? $language->title_native : $langCode;
-							?>
-						<?php endif; ?>
-					</a>
-				</li>
-			<?php endif; ?>
-		<?php endforeach; ?>
-	</ul>
-<?php endif; ?>
 
-<?php if ($footerText) : ?>
-	<div class="posttext"><p><?php echo $footerText; ?></p></div>
-<?php endif; ?>
+<ul>
+	<?php foreach ($list as $language): ?>
+		<?php if ($params->get('show_active', 0) || !$language->active): ?>
+			<li class="<?php echo $language->active ? 'active' : ''; ?>" dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr' ?>"><a href="<?php echo $language->link; ?>"><?php
+					$langCode = $language->sef == "fa" ? 'فا' : $language->sef;
+					echo $params->get('full_name', 1) ? $language->title_native : $langCode;
+					?></a></li>
+		<?php endif; ?>
+<?php endforeach; ?>
+</ul>
