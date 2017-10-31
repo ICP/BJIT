03bd56f7457f2f2eea51af83eded952e8a482b04
Farid Rn <faridv@gmail.com>
2017-10-31 22:02:42 +0330

    Added every damn possible favicons!
    Rahafar

--- templates/rahafar/blocks/header.php
+++ templates/rahafar/blocks/header.php
@@ -30,6 +30,26 @@ JResponse::clearHeaders();
 			<meta property="og:image:width" content="1920">
 			<meta property="og:image:height" content="1080">
 		<?php } ?>
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/img/rahafar/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/img/rahafar/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/img/rahafar/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/img/rahafar/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/img/rahafar/apple-touch-icon-60x60.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/img/rahafar/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/img/rahafar/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/img/rahafar/apple-touch-icon-152x152.png" />
+		<link rel="icon" type="image/png" href="/assets/img/rahafar/favicon-196x196.png" sizes="196x196" />
+		<link rel="icon" type="image/png" href="/assets/img/rahafar/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/png" href="/assets/img/rahafar/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="/assets/img/rahafar/favicon-16x16.png" sizes="16x16" />
+		<link rel="icon" type="image/png" href="/assets/img/rahafar/favicon-128.png" sizes="128x128" />
+		<meta name="application-name" content="Rahafar"/>
+		<meta name="msapplication-TileColor" content="#0ebee3" />
+		<meta name="msapplication-TileImage" content="/assets/img/rahafar/mstile-144x144.png" />
+		<meta name="msapplication-square70x70logo" content="/assets/img/rahafar/mstile-70x70.png" />
+		<meta name="msapplication-square150x150logo" content="/assets/img/rahafar/mstile-150x150.png" />
+		<meta name="msapplication-wide310x150logo" content="/assets/img/rahafar/mstile-310x150.png" />
+		<meta name="msapplication-square310x310logo" content="/assets/img/rahafar/mstile-310x310.png" />
 		<?php
 		$JHeader = $this->getHeadData(); // Get Joomla Native Head tags
 		$this->setHeadData($helper->cleanHead($JHeader)); // Removing unwanted tags from Joomla native head
